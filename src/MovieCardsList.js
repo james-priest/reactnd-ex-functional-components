dcc3f3a94801a530b693e487947f0496abf39587
James Priest <james-priest@outlook.com>
2019-01-18 10:42:06 +0000

    refactor: From classes to functional components

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
+function MovieCardsList(props) {
+  const { profiles, users, movies } = props;
+  const usersByMovie = {};
 
-    const { profiles, users, movies } = this.props;
-    const usersByMovie = {};
+  profiles.forEach(profile => {
+    const movieID = profile.favoriteMovieID;
 
-    profiles.forEach(profile => {
-      const movieID = profile.favoriteMovieID;
+    if (usersByMovie[movieID]) {
+      usersByMovie[movieID].push(profile.userID);
+    } else {
+      usersByMovie[movieID] = [profile.userID];
+    }
+  });
 
-      if (usersByMovie[movieID]) {
-        usersByMovie[movieID].push(profile.userID);
-      } else {
-        usersByMovie[movieID] = [profile.userID];
-      }
-    });
+  console.log('usersByMovie:', usersByMovie);
 
-    const movieCards = Object.keys(movies).map(id => (
-      <MovieCard
-        key={id}
-        users={users}
-        usersWhoLikedMovie={usersByMovie[id]}
-        movieInfo={movies[id]}
-      />
-    ));
+  console.log('Object.keys(movies):', Object.keys(movies));
+  const movieCards = Object.keys(movies).map(id => {
+    console.log('------------------');
+    console.log('id', id);
+    console.log('users', users);
+    console.log('usersByMovie[id]', usersByMovie[id]);
+    console.log('movies[id]', movies[id]);
+    
+    return <MovieCard
+      key={id}
+      users={users}
+      usersWhoLikedMovie={usersByMovie[id]}
+      movieInfo={movies[id]}
+    />
+});
 
-    /*
-    Return JSX
-    */
-    return <ul>{movieCards}</ul>;
-  }
+  return <ul>{movieCards}</ul>;
 }
 
 export default MovieCardsList;
